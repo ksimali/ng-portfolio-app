c2c47a63d0c768d8c9a4541073e1f80175ca07df
ksimali <cassim.ali@outlook.fr>
2025-06-29 19:59:58 -0400

    Add three other path in app-routing module: - path for Portfolio, Resume and Contact components

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Portfolio } from './portfolio/portfolio';
+import { Resume } from './resume/resume';
+import { Contact } from './contact/contact';
 
 const routes: Routes = [
   {path: 'home', component: Home},
+  {path: 'portfolio', component: Portfolio},
+  {path: 'resume', component: Resume},
+  {path: 'contact', component: Contact}
 ];
 
 @NgModule({
