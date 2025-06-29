0815a1bdce1ab8781dced54d4348489f893e9b57
ksimali <cassim.ali@outlook.fr>
2025-06-29 17:03:32 -0400

    Add Components for the routes : -Generate home, portfolio, resume and contact components

---
+++ src/app/portfolio/portfolio.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  standalone: false,
+  templateUrl: './portfolio.html',
+  styleUrl: './portfolio.scss'
+})
+export class Portfolio {
+
+}
---
+++ src/app/resume/resume.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume',
+  standalone: false,
+  templateUrl: './resume.html',
+  styleUrl: './resume.scss'
+})
+export class Resume {
+
+}
