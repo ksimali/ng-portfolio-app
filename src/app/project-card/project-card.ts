4e7a9e30222a5e59733b3e8742f14886126695c0
ksimali <cassim.ali@outlook.fr>
2025-06-30 12:12:02 -0400

    Passing data Into the parent component portfolio.html : -Define an input in project-card typeScript -Use the value of the input in project-card template -Pass an unique project name in portfolio template

--- src/app/project-card/project-card.ts
+++ src/app/project-card/project-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './project-card.scss'
 })
 export class ProjectCard {
-
+  @Input() projectName: string = "";
 }
