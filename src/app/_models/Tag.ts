29a809ad45997bfdc10d442ad460e82484124217
ksimali <cassim.ali@outlook.fr>
2025-07-02 15:26:36 -0400

    Change Javascript color  in JAVASCRIPT attributes in Tag Class

--- src/app/_models/Tag.ts
+++ src/app/_models/Tag.ts
@@ -7,7 +7,7 @@ export class Tag {
     static readonly JAVA = new Tag('Java', 'orange');
     static readonly NODEJS = new Tag('Node.JS', 'brown');
     static readonly ASPNET = new Tag('ASP.NET', 'purple');
-    static readonly JAVASCRIPT = new Tag('Javascript', 'yellow');
+    static readonly JAVASCRIPT = new Tag('Javascript', 'gold');
     static readonly REACT = new Tag('React', 'blue');
 
     //constructor
