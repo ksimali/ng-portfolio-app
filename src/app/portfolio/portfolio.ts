68e5366daa0d0cff8e413921723d4db3dcfd6352
ksimali <cassim.ali@outlook.fr>
2025-07-04 11:53:33 -0400

    Setup filter for Angular Framework : -Define a angular boolean set to false in portfolio.ts & Update filter() function to add angular -Add input & label tag for angular filter in portfolio.html

--- src/app/portfolio/portfolio.ts
+++ src/app/portfolio/portfolio.ts
@@ -17,8 +17,9 @@ export class Portfolio implements OnInit {
   // a variable to keep track whether or Filter section is collapsed or not
   isCollapsed: boolean = true; // isCollapse is true by default
 
-  //Define a boolean typescript set to false
+  //Define a typescript boolean  set to false
   typescript: boolean = false;
+  angular: boolean = false;
 
   constructor(private titleService: Title, private projectService: ProjectService) {
     this.titleService.setTitle('Cassim Ali - Portfolio');
@@ -37,6 +38,9 @@ export class Portfolio implements OnInit {
 
     if(this.typescript) {
       filterTags.push(Tag.TYPESCRIPT);
+    }
+    if(this.angular) {
+      filterTags.push(Tag.ANGULAR);
     }
 
     this.projects = this.projectService.getProjectByFilter(filterTags);
